5a4015f1bde3a9a738ecfbf6c3dcbdca385fbc60
Remi Gau <remi_gau@hotmail.com>
2023-05-29 18:09:45 -0400

    switch to hatch

--- bids/ext/reports/__init__.py
+++ bids/ext/reports/__init__.py
@@ -14,8 +14,4 @@ due.cite(
 
 del due, Doi
 
-__version__ = _version.get_versions()["version"]
-
-from . import _version
-
-__version__ = _version.get_versions()["version"]
+__version__ = _version.__version__
--- setup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from setuptools import setup
-import versioneer
-
-setup(
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-)
