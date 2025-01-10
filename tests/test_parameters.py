3a4183e3170df80a86e4111a957fc1b2348b8ffd
Remi Gau <remi_gau@hotmail.com>
2025-01-10 10:49:16 +0100

    [FIX] extract tests (#107)
    * extract tests
    
    * run on proper folder
    
    * clean
    
    * rm scope fixture

---
+++ tests/test_parameters.py
@@ -0,0 +1,129 @@
+"""Tests for bids.reports.parameters module."""
+
+from __future__ import annotations
+
+import pytest
+
+from bids.ext.reports import parameters
+
+
+@pytest.mark.parametrize(
+    "ScanningSequence, expected_seq",
+    [
+        ("EP", "echo planar (EP)"),
+        ("GR", "gradient recalled (GR)"),
+        ("IR", "inversion recovery (IR)"),
+        ("RM", "research mode (RM)"),
+        ("SE", "spin echo (SE)"),
+        ("SE_EP", "spin echo and echo planar (SE/EP)"),
+        ("spam egg", "UNKNOWN SEQUENCE"),
+    ],
+)
+def test_sequence(ScanningSequence, expected_seq, testconfig):
+    """test for sequence and variant type description"""
+    metadata = {
+        "ScanningSequence": ScanningSequence,
+    }
+    seqs = parameters.sequence(metadata, testconfig)
+    assert seqs == expected_seq
+
+
+@pytest.mark.parametrize(
+    "SequenceVariant, expected_var",
+    [
+        ("MP", "MAG prepared"),
+        ("MTC", "magnetization transfer contrast"),
+        ("NONE", "no sequence variant"),
+        ("OSP", "oversampling phase"),
+        ("SK", "segmented k-space"),
+        ("SS", "steady state"),
+        ("TRSS", "time reversed steady state"),
+        ("MP_SS", "MAG prepared and steady state"),
+        ("spam", "UNKNOWN SEQUENCE VARIANT"),
+    ],
+)
+def test_variants(SequenceVariant, expected_var, testconfig):
+    """test for sequence and variant type description"""
+    metadata = {
+        "SequenceVariant": SequenceVariant,
+    }
+    variants = parameters.variants(metadata, testconfig)
+    assert variants == expected_var
+
+
+def test_bvals_smoke(testlayout):
+    """Smoke test for parsing _dwi.bval
+
+    It should return a str description when provided valid inputs.
+    """
+    bval_file = testlayout.get(
+        subject="01",
+        session="01",
+        suffix="dwi",
+        extension=[".bval"],
+    )
+
+    bval_str = parameters.bvals(bval_file[0])
+    assert isinstance(bval_str, str)
+
+
+def test_echo_times_fmap(testlayout):
+    """Smoke test for parsing echo time
+
+    It should return a str description when provided valid inputs.
+    """
+    fmap_file = testlayout.get(
+        subject="01",
+        session="01",
+        suffix="phasediff",
+        extension=[".nii.gz"],
+    )
+
+    te_1, _ = parameters.echo_times_fmap(fmap_file)
+    assert isinstance(te_1, float)
+
+
+def test_describe_func_duration_smoke():
+    # given
+    n_vols = 100
+    tr = 2.5
+    # when
+    duration = parameters.func_duration(n_vols, tr)
+    # then
+    expected = "4:10"
+    assert duration == expected
+
+
+@pytest.mark.parametrize(
+    "slice_times, expected",
+    [
+        ((1, 2, 3, 4), "sequential ascending"),
+        ([4, 3, 2, 1], "sequential descending"),
+        ([1, 3, 2, 4], "interleaved ascending"),
+        ([4, 2, 3, 1], "interleaved descending"),
+    ],
+)
+def test_get_slice_info(slice_times, expected):
+    slice_order_name = parameters.get_slice_info(slice_times)
+    assert slice_order_name == expected
+
+
+def test_slice_timing(testmeta):
+    slice_str = parameters.slice_order(testmeta)
+    expected = " in sequential ascending order"
+    assert slice_str == expected
+
+
+def test_intendedfor_targets(testmeta_light, testlayout):
+    intended_for = parameters.intendedfor_targets(testmeta_light, testlayout)
+    assert intended_for == ""
+
+    fmap_files = testlayout.get(
+        subject="01",
+        session="01",
+        suffix="phasediff",
+        extension=[".nii.gz"],
+    )
+    metadata = fmap_files[0].get_metadata()
+    intended_for = parameters.intendedfor_targets(metadata, testlayout)
+    assert intended_for == "first and second runs of the N-Back BOLD scan"
---
+++ tests/test_parsing.py
@@ -0,0 +1,119 @@
+"""Tests for bids.reports.parsing."""
+
+from __future__ import annotations
+
+from bids.layout import BIDSLayout
+
+from bids.ext.reports import parsing
+
+
+def test_institution_info(testlayout):
+    files = testlayout.get(
+        subject="01",
+        session="01",
+        extension=[".nii.gz"],
+    )
+    desc = parsing.institution_info(files)
+    assert desc == ""
+
+
+def test_anat_info_smoke(testlayout, testconfig):
+    """Smoke test for parsing.anat_info.
+
+    It should return a str description when provided valid inputs.
+    """
+    anat_files = testlayout.get(
+        subject="01",
+        session="01",
+        suffix="T1w",
+        extension=[".nii.gz"],
+    )
+    desc = parsing.anat_info(anat_files, testconfig, testlayout)
+    assert isinstance(desc, str)
+
+
+def test_dwi_info_smoke(testlayout, testconfig):
+    """Smoke test for parsing.dwi_info.
+
+    It should return a str description when provided valid inputs.
+    """
+    dwi_files = testlayout.get(
+        subject="01",
+        session="01",
+        datatype="dwi",
+        extension=[".nii.gz"],
+    )
+    desc = parsing.dwi_info(dwi_files, testconfig, testlayout)
+    assert isinstance(desc, str)
+
+
+def test_fmap_info_smoke(testlayout, testconfig):
+    """Smoke test for parsing.fmap_info.
+
+    It should return a str description when provided valid inputs.
+    """
+    fmap_files = testlayout.get(
+        subject="01",
+        session="01",
+        datatype="fmap",
+        suffix="phasediff",
+        extension=[".nii.gz"],
+    )
+    desc = parsing.fmap_info(fmap_files, testconfig, testlayout)
+    assert isinstance(desc, str)
+
+
+def test_func_info_smoke(testlayout, testconfig):
+    """Smoke test for parsing.func_info.
+
+    It should return a str description when provided valid inputs.
+    """
+    func_files = testlayout.get(
+        subject="01",
+        session="01",
+        task="nback",
+        run="01",
+        extension=[".nii.gz"],
+    )
+    desc = parsing.func_info(func_files, testconfig, testlayout)
+    assert isinstance(desc, str)
+
+
+def test_meg_info_smoke(data_path):
+    """Smoke test for parsing.meg_info.
+
+    It should return a str description when provided valid inputs.
+    """
+    data_dir = data_path / "ds000117"
+    testlayout = BIDSLayout(data_dir)
+    meg_files = testlayout.get(
+        subject="01",
+        session="meg",
+        task="facerecognition",
+        run="01",
+    )
+    desc = parsing.meg_info(meg_files)
+    assert isinstance(desc, str)
+
+
+def test_final_paragraph_smoke(testmeta):
+    """Smoke test for parsing.final_paragraph.
+
+    It should return a str description when provided valid inputs.
+    """
+    desc = parsing.final_paragraph(testmeta)
+    assert isinstance(desc, str)
+
+
+def test_parse_files_smoke(testlayout, testconfig):
+    """Smoke test for parsing.parse_files.
+
+    It should return a list of string descriptions when provided valid inputs,
+    with each string containing the description for a single nifti file
+    (except functional data, which is combined within task, across runs).
+    """
+    subject = "01"
+    niftis = testlayout.get(subject=subject, extension=[".nii", ".nii.gz"])
+    desc = parsing.parse_files(testlayout, niftis, testconfig)
+    assert isinstance(desc, list)
+    assert isinstance(desc[0], str)
